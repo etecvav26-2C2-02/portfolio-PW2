35326c27d918f7478646691d0b26b46a0f894079
Eliaskerche <kercheelias464@gmail.com>
2026-03-28 11:57:01 -0300

    Update main.php

--- exercicio8/main.php
+++ exercicio8/main.php
@@ -8,10 +8,12 @@
         return $rs;
         // retorna a função
     }
-    
+
     if(isset($_POST["n1"])) {
+        //verifica se o n1 foi "lançado" pelo form
         $n1 = $_POST["n1"];
         $rs = soma($n1, 0);
         echo "Resultado: ". $rs;
+        // Mostra o resultado da soma
     }
 ?>
