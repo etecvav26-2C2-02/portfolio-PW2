0eed486b3389bf529835b0d7320a567e62705667
DouglasTeles576 <douglasteles011@gmail.com>
2026-03-29 00:28:40 -0300

    adicao do include

--- php4/triangulo.php
+++ php4/triangulo.php
@@ -16,33 +16,3 @@ if (isset($_POST['valor'])) {
     }
 }
 ?>
-
-
-=======
-<?php
-//checagem da caixa de texto
-if (isset($_POST['valor'])) {
-    $n = $_POST['valor'];
-    //esquema de repetição
-    for ($i = 1; $i <= $n; $i++) {
-        //impressora de números
-        for ($j = 1; $j <= $i; $j++) {
-            echo $j . " ";
-        }
-        echo "<br>";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.php">voltar</a>
-</body>
->>>>>>> d3b6fe0aeddd44f52ee6a5abe1c40e9f8f325847
-</html>
