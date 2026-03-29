004a5998ec32c1c9bcd79d6936b91cef93118d64
DouglasTeles576 <douglasteles011@gmail.com>
2026-03-29 00:36:44 -0300

    1.1

--- php6/fibonacci.php
+++ php6/fibonacci.php
@@ -1,15 +1,15 @@
 <?php
 
 include('index.php');
-
+//funcao que faz tudo funcinar
     function fibonacci($f){
         $n1 = 0;
         $n2 = 1;
         $result = 1;
-
+//escrita da sequencia em casos especificos
         if($f >= 1) echo $n1 . " ";
         if($f >= 2) echo $n2 . " ";
-
+//escrita geral
         for($i = 3; $i <= $f; $i++){
             $result = $n1 + $n2;
             echo $result . " ";
@@ -17,6 +17,7 @@ include('index.php');
             $n2 = $result;
         }
     }
+//resultado impresso
     if(isset($_POST["n"])){
         $f = $_POST["n"];
         echo fibonacci($f);
