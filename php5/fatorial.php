3bf58034049e4995b48e4f1577bbd7863cd80332
DouglasTeles576 <douglasteles011@gmail.com>
2026-03-29 00:25:40 -0300

    adicao do include

--- php5/fatorial.php
+++ php5/fatorial.php
@@ -1,5 +1,8 @@
 <<<<<<< HEAD
 <?php
+
+include ("index.php");
+
 //função que faz o fatorial
     function fat($num){
         $result = 1;
@@ -30,59 +33,3 @@
         echo "<br>";
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.php">voltar</a>
-</body>
-=======
-<?php
-//função que faz o fatorial
-    function fat($num){
-        $result = 1;
-        for($i = 1; $i <= $num; $i++){
-            $result = $result * $i;
-        }
-         return $result;   
-    }
-
- $soma = 0;
-
-    if(isset($_POST["n1"])){
-//pega varias variaveis
-            $valores = [
-                $_POST["n1"],
-                $_POST["n2"],
-                $_POST["n3"],
-                $_POST["n4"],
-                $_POST["n5"]
-            ];
-
-    //le a lista de variaveis
-        foreach ($valores as $n){
-    //corre pela lista $valores usando cada um dos valores
-            $soma = fat($n) + $soma;
-        }
-        echo "seu valor é " . $soma;
-        echo "<br>";
-    }
-?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="index.php">voltar</a>
-</body>
->>>>>>> d3b6fe0aeddd44f52ee6a5abe1c40e9f8f325847
-</html>
