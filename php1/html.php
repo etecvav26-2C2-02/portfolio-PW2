de0a42cad3efe8ef42039935affe92b3e15b7ae6
etecvav26-2C2-02 <douglasteles010@gmail.com>
2026-04-09 17:07:18 -0300

    Add files via upload

---
+++ php1/html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="trabalho1.php" method="POST">
+    <input type="text" name="uni" placeholder="C ou F">
+    <input type="number" name="n">
+    <button type="submit">calcular</button>
+    </form>
+</body>
+</html>
